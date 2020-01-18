22a722a46623afa4b722cec6a5b909389968c00a
Ivo Krastev <52259854+BrascoBG@users.noreply.github.com>
2020-01-18 13:28:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ function profitIvan([arg1,arg2,arg3]){
   let taxes = yearSalaryBonus * 0.25;
   let salaryNetUsd = yearSalaryBonus - taxes;
   let salaryNetBgn = salaryNetUsd * usdBgn;
-  console.log((salaryNetBgn / 365).toFixed(2));
+  
+  console.log("Your average NET salary after taxes is " + (salaryNetBgn / 365).toFixed(2) + " per day.");
 }
 
 profitIvan([15,105,1.71]);
